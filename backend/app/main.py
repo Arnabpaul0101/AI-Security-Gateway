9e690d0b9a7322fad827fedb6f987308c042ca4b
Arnabpaul0101 <arnabpaul01012004@gmail.com>
2026-04-14 03:07:03 +0530

    allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="AI Secure Data Intelligence Platform")
 
 origins = [
-    "http://localhost:5173", 
+    "*", 
 ]
 
 app.add_middleware(
